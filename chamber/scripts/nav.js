8d45624d7378455e8b16a697c12a358881f6057a
Michael Mwanza <mwanza.n.m@gmail.com>
2025-03-18 00:58:36 +0200

    dynamic nav removed

--- chamber/scripts/nav.js
+++ chamber/scripts/nav.js
@@ -1,16 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const nav = document.createElement('nav');
-    nav.innerHTML = `
-        <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="discover.html">Discover</a></li>
-            <li><a href="directory.html">Directory</a></li>
-            <li><a href="join.html">Join</a></li>
-        </ul>
-    `;
-    const header = document.querySelector('header');
-    header.appendChild(nav);
-
     const hamburger = document.querySelector('.hamburger');
     const navMenu = document.querySelector('nav');
 
--- chamber/scripts/navigation.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.querySelector('.menu-icon');
-    const nav = document.querySelector('nav ul');
-    
-    menuIcon.addEventListener('click', function() {
-        nav.classList.toggle('active');
-    });
-});
